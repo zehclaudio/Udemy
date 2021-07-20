047ee02a63f06790196c54b42927ac6edcc1021c
José Claudio Alvarez Junior <zeh.claudio@hotmail.com>
2021-07-20 18:49:31 -0300

    Delete 50 Projects In 50 Days - HTML, CSS & JavaScript directory

--- 50 Projects In 50 Days - HTML, CSS & JavaScript/Day 8 - Form Wave Animation/script.js
+++
@@ -1,8 +0,0 @@
-const labels = document.querySelectorAll('.form-control label');
-
-labels.forEach(label => {
-    label.innerHTML = label.innerText
-        .split('')
-        .map((letter, i) => `<span style="transition-delay: ${i * 50}ms;">${letter}</span>`)
-        .join('')
-})
